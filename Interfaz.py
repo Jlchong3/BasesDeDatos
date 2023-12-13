43641cddc8f2250f50ed50e4110fa6debaf02a21
Jose Luis <jchongac@fiec.espol.edu.ec>
2023-12-12 21:21:28 -0500

    primer commit

---
+++ Interfaz.py
@@ -0,0 +1,49 @@
+import pandas as pd
+import mysql.connector as sql
+
+#funciones
+
+def inputValido(opciones):
+    opcion = input("Eliga una opción: \n")
+    while opcion not in opciones:
+        opcion = input("Eliga una opción: ")
+    return opcion
+
+def imprimirTabla(cursor, tabla):
+    cursor.execute(f"Select * from {tabla}")
+    rows = cursor.fetchall()
+    columns = [i[0] for i in cursor.description]  # Extracting column names from the cursor
+    df = pd.DataFrame(rows, columns=columns)
+    print(df)
+    del df
+
+conn = sql.connect(
+    host = "localhost",
+    user = "root",
+    password = "",
+    database = ""
+)
+
+cursor = conn.cursor()
+
+opciones = list("123")
+opcion = ""
+while opcion != "3":
+    print('''
+Opciones:
+  1. Tabla de Ganadores
+  2. Table de Sorteos
+  3. salir
+    ''')
+    opcion = inputValido(opciones)
+    match opcion:
+        case "1":
+            imprimirTabla(cursor, "ganador")
+        case "2":
+            imprimirTabla(cursor, "sorteo")
+        case _:
+            print("Sesión terminada con exito.")
+
+
+
+
