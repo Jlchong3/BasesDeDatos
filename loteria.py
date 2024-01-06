3dd5d8849594a404df56bfb72b09137d5765c635
Alex <licazago@fiec.espol.edu.ec>
2024-01-05 23:17:17 -0500

    conexion base en nube

---
+++ loteria.py
@@ -0,0 +1,21 @@
+import mysql.connector
+from mysql.connector import Error
+
+
+def create_connection(host_name, user_name, user_password, db_name, db_port):
+    connection = None
+    try:
+        connection = mysql.connector.connect(
+            host=host_name,
+            user=user_name,
+            passwd=user_password,
+            database=db_name,
+            port=db_port
+        )
+        print("Connection to MySQL DB successful")
+    except Error as e:
+        print(f"The error '{e}' occurred")
+    return connection
+
+
+conexion = create_connection("basesloteria.mysql.database.azure.com", "administrador", "@basesloteriaN", "loterianacional", 3306) 
